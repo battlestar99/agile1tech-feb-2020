fd1a81ccdc838f5b6dd90d947b2204c53d2ecd24
Razin Anik <scrumagile24@gmail.com>
2020-04-22 20:55:59 -0400

    TestNG Class 2

--- TestNGScripts/src/scripts/elementCheck.java
+++ TestNGScripts/src/scripts/elementCheck.java
@@ -2,8 +2,10 @@ package scripts;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -12,6 +14,8 @@ import org.testng.annotations.Test;
 
 public class elementCheck {
 	
+	
+	int x; 
 	WebDriver driver;
 	pageHome ph; 
 	
@@ -21,7 +25,9 @@ public class elementCheck {
 		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\rznan\\Desktop\\Feb_WS\\libraries\\chromedriver.exe");
 			
 			
-			driver = new ChromeDriver();
+		 
+		 x = 15; 
+			 driver = new ChromeDriver();
 			
 			 ph= new pageHome(driver);
 			
@@ -29,13 +35,30 @@ public class elementCheck {
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
-	@Test
+	@Test(priority=1)
 	public void validateElementPresent() throws InterruptedException {
 		
 		Thread.sleep(2000);
 		ph.getFirstName().sendKeys("Michael");
 		ph.getLastName().sendKeys("Scott");
+		
+		boolean x = ph.getFirstName().isDisplayed();
+		Assert.assertTrue(x);
 		Thread.sleep(2000);
+	}
+	
+	
+	@Test(priority=2)
+public void validateMessangerURL() throws InterruptedException {
+		
+		driver.findElement(By.linkText("Messenger")).click();
+		
+		String expectedURL = "https://www.mess.com/";
+		String actualURL= driver.getCurrentUrl();
+		
+		Assert.assertEquals(actualURL, expectedURL);
+		
+		
 	}
 	
 	@AfterTest
